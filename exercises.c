66c2fb324518d9667b09c88709ebc9659977f77b
Your Name <you@example.com>
2024-03-16 03:32:33 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -105,38 +105,35 @@ y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
   0 si no está ordenado, y -1 si está ordenado en orden descendente.
 */
 int checkSorted(int arr[], int size) { 
-  int ascen = arr[0];
-  int contup = 1;
-  int desce = arr[0];
-  int contdown = 1;
-  
-  for (int i = 0; i < size; i++)
-    {
-      if (arr[i] >= ascen)  
-      { 
-        ascen = arr[i];
-        contup++;
-      }
-
-      else if (arr[i] <= desce)
-      {
-        desce = arr[i];
-        contdown++;
-      }
+    if (size <= 1) {
+        return 1;
     }
 
-  if (contup == size)
-  {
-    return 1;
-  }
+    int isAscending = 1;
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < arr[i - 1]) {
+            isAscending = 0; 
+            break;
+        }
+    }
+    if (isAscending) {
+        return 1; 
+    }
 
-  else if (contdown == size)
-  {
-    return -1;
-  }
-  
-  return 0; 
+    int isDescending = 1;
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > arr[i - 1]) {
+            isDescending = 0; 
+            break;
+        }
+    }
+    if (isDescending) {
+        return -1; 
+    }
+
+    return 0; 
 }
+
 
 /*
 Ejercicio 6: Información de una Biblioteca
